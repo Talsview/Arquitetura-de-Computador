e74263698cc0624c298ab641013382aab391b747
Talsview <daviconcyline@gmail.com>
2025-05-14 21:49:08 -0300

    Add files via upload

--- Senai_LLP_HelloWorld_Java/HelloWorldInteracao.java
+++ Senai_LLP_HelloWorld_Java/HelloWorldInteracao.java
@@ -1,1 +1,10 @@
-"" 
+class HelloWorldInteracao{
+
+    public static void main(String[] args){
+        //Ler o nome
+        String nome = "Davi";
+        //Imprimir mensagem
+        System.out.println("Hello World "+ nome);
+
+    }
+}
